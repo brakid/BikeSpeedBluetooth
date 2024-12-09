0f3b7d8626133ea3fcbbfb74fc64a82ce189992c
Hagen Schupp <hagen.schupp@gmail.com>
2024-12-09 07:55:43 +0100

    Correcting power curve: was overestimating power
    Speed: 29kmh
    Measured Power using the Elite App: 176 watts
    Calculated Power using the cubic-polynomial: 195 watts (overestimating bzy 20 watts=
    
    Switched to quartic-polynomial with lower error residual

--- src/trainer.go
+++ src/trainer.go
@@ -9,7 +9,7 @@ func (t *Trainer) CalculatePower(speedKmh float64, cadenceRpm float64) float64 {
 		return 0.0
 	}
 
-	power := -0.00009*math.Pow(speedKmh, 3) + 0.1173*math.Pow(speedKmh, 2) + 3.437*speedKmh - 1.236
+	power := 0.00004667*math.Pow(speedKmh, 4) - 0.005437*math.Pow(speedKmh, 3) + 0.2577*math.Pow(speedKmh, 2) + 2.050*speedKmh
 
 	if power < 0 {
 		return 0.0
